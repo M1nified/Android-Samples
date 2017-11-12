f2772361740f733a2aac670403057790e0715b79
Minified <gora.pub@gmail.com>
2017-11-12 20:25:00 +0100

    added on click listener via xml

--- app/src/main/java/com/m1nified/android_samples/MainActivity.kt
+++ app/src/main/java/com/m1nified/android_samples/MainActivity.kt
@@ -2,11 +2,17 @@ package com.m1nified.android_samples
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonOnClick(v: View){
+        Log.d(this.javaClass.name.toString(), "BUTTON CLICK XML")
     }
 }
