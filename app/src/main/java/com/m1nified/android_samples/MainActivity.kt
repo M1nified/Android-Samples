c809056b23081b0a4ca18871f96b0092885fac5d
Minified <gora.pub@gmail.com>
2017-11-12 20:29:07 +0100

    added on click listener via onCreate

--- app/src/main/java/com/m1nified/android_samples/MainActivity.kt
+++ app/src/main/java/com/m1nified/android_samples/MainActivity.kt
@@ -2,11 +2,17 @@ package com.m1nified.android_samples
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            Log.d(this.javaClass.name.toString(), "BUTTON CLICK")
+        }
     }
 }
